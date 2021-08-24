54b77fbe7c12d47f18d78060ca31f20d424b00b0
wladyslaw <wladyslaw@jellinek.pl>
2021-08-24 09:56:31 +0200

    Automatic implementation a link(Clear cache) to the menu

--- src/Menu/AdminMenu.php
+++ src/Menu/AdminMenu.php
@@ -2,7 +2,14 @@
 
 namespace TwinElements\ClearCacheBundle\Menu;
 
-class AdminMenu
-{
+use TwinElements\AdminBundle\Menu\AdminMenuInterface;
 
+class AdminMenu implements AdminMenuInterface
+{
+    public function getItems()
+    {
+        return [
+            MenuItem::newInstance('te_clear_cache.clear_website_cache', 'te_clear_cache', [], 150, AdminUserRole::ROLE_ADMIN),
+        ];
+    }
 }
